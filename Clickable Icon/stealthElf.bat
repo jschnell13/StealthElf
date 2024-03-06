9941673f868eaf49fd17545f9b6ad8a7450a2540
jschnell13 <122713680+jschnell13@users.noreply.github.com>
2024-03-06 12:22:48 -0600

    Updated stealthElf.bat and added JAR file

--- Clickable Icon/stealthElf.bat
+++ Clickable Icon/stealthElf.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 set CLASSPATH=.
-set CLASSPATH=%CLASSPATH%;C:\Users\Jordan\Desktop\Spring 2024\CYBR 404\StealthElf\test\out\artifacts\test_jar\test.jar
+::Change path to location of StealthElf.jar on whatever computer is being used
+set CLASSPATH=%CLASSPATH%;C:\Users\Jordan\IdeaProjects\StealthElf\out\artifacts\StealthElf_jar\StealthElf.jar
 
 start cmd /k ""%JAVA_HOME%\bin\java" -Xms128m -Xmx384m -Xnoclassgc Main"
